7875dbd3113fb13851116a98e376427de8a47489
amelianshan <shanshan_amelian@hotmail.com>
2022-02-13 17:43:01 -0500

    commit a script

---
+++ GetDirectReports/GetDirectReports.ps1
@@ -0,0 +1,48 @@
+﻿#Connect-AzureAD
+#Connect-MsolService
+
+
+
+Function getDirectreport($name){
+
+    $directReports = @()
+    $objectID = get-msoluser -UserPrincipalName $name | Select-Object -expandproperty ObjectID
+
+
+   
+
+    
+    $directReportNames = Get-AzureADUserDirectReport -ObjectId $objectID | Select-Object -ExpandProperty UserPrincipalName
+
+    $directReports += $directReportNames
+
+
+     if($directReports.Count -eq 0){
+        
+        return
+
+    }else{
+
+        Write-Host $name, $directReportNames
+        
+        $directReportNames |   Out-File -Append c:\result.csv
+
+        foreach($directReport in $directReports){
+
+            
+           getDirectreport($directReport)
+
+
+
+        }
+
+
+
+
+    }
+
+    
+
+}
+
+getDirectreport("hua.huang@fountain-med.com")
