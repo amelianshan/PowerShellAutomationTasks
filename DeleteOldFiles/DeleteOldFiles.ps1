ff19588b9bab9679526e6d3eadf090a0e0c48ab5
amelianshan <shanshan_amelian@hotmail.com>
2022-02-13 17:29:36 -0500

    commit a script

---
+++ DeleteOldFiles/DeleteOldFiles.ps1
@@ -0,0 +1,7 @@
+﻿# Delete all Files(No folder) in D:\ older than 90 day(s)
+$Path = "D:\"
+$Daysback = "-90"
+ 
+$CurrentDate = Get-Date
+$DatetoDelete = $CurrentDate.AddDays($Daysback)
+Get-ChildItem -File $Path -Recurse  | Where-Object { $_.LastWriteTime -lt $DatetoDelete } 
